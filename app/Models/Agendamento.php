575723960a02b1cd948ac93710ab8222abc99b3a
Maxwell Diniz <max220893@gmail.com>
2022-02-24 23:47:58 -0300

    [master] update 85 files and delete 3 files

---
+++ app/Models/Agendamento.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Agendamento extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'especialidade_int',
+        'usuario_id',
+        'barbeiro_id',
+        'data'
+    ];
+
+    public function especialidade()
+    {
+        return $this->belongsTo(Especialidade::class, 'especialidade_int');
+    }
+
+    public function usuario()
+    {
+        return $this->belongsTo(Usuario::class, 'usuario_id');
+    }
+
+    public function barbeiro()
+    {
+        return $this->belongsTo(Barbeiro::class, 'barbeiro_id');
+    }
+}
--- database/migrations/2022_02_24_231750_create_usuarios_table.php
+++ database/migrations/2022_02_24_231750_create_usuarios_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('email');
+            $table->string('senha');
+            $table->unsignedTinyInteger('admin');
         });
     }
 
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('usuarios');
     }
 };
